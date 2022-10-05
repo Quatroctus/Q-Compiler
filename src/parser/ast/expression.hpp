f7086f04061fff011f68abb023c7ce60af8bdae3
JamesTaylor <tayloja5@oregonstate.edu>
2022-10-05 04:45:18 -0700

    Misc. Comment cleanup.

--- src/parser/ast/expression.hpp
+++ src/parser/ast/expression.hpp
@@ -12,11 +12,10 @@
 #include <llvm-13/llvm/IR/Value.h>
 
 struct ExpressionNode {
-    // TODO:
     virtual ~ExpressionNode() { }
 
     virtual TypedValue llvmEvaluate(QContext& qctx, llvm::LLVMContext& ctx, llvm::IRBuilder<>& builder) const = 0;
     virtual void staticAnalysis(const QContext& qctx, std::optional<Type>& expected) = 0;
 
-    virtual Type getType() const = 0;
+    virtual Type getType() const = 0; // TODO: Add QContext parameter.
 };
